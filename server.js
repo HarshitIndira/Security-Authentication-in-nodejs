1b2569fc6a8ac9474af250aa09f4ca89cbeb1686
HarshitIndira <sharshit@indiratrade.com>
2024-04-23 18:26:30 +0530

    Integrated openssl

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
+const fs = require('fs');
+const https = require('https');
 const express = require('express');
 const path = require('path');
+const helmet = require('helmet');
+const { error } = require('console');
 
 const app = express();
 
-app.get('/secure', (req, resp) => {
+app.use(helmet());
+
+function checkLoggedIn(res, resp, next) {
+    const isLoggedIn = true;
+    if (!isLoggedIn) {
+        return res.statusCode(401).json({
+            error: "You must log in",
+        })
+    }
+    next();
+}
+
+app.get('/secure', checkLoggedIn, (req, resp) => {
     resp.send("Your secret key is : 7");
 })
 
@@ -11,6 +27,9 @@ app.get('/', (req, resp) => {
     resp.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(3000, () => {
+https.createServer({
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem'),
+}, app).listen(3000, () => {
     console.log("Running on port : 3000");
 })
